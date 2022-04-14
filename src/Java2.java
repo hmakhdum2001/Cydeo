a519f283d0630172ee44a66fa7763f9515b05a74
hmakh <hmakhdum2001@yahoo.com>
2022-04-14 17:34:50 +0100

    Initial commit

---
+++ src/Java2.java
@@ -0,0 +1,14 @@
+public class Java2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+        System.out.println("This is my 1st Github Project");
+
+
+
+    }
+
+
+}
